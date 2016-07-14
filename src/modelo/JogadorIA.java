e4edac2499e063ba93c577e6d67861081602aae1
yosh <yosh@yosh-Inspiron-5458>
2016-07-14 16:16:25 -0400

    Switch para calcular a quantidade de cartasPossiveis de cada cor

--- src/modelo/JogadorIA.java
+++ src/modelo/JogadorIA.java
@@ -20,17 +20,33 @@ public class JogadorIA extends Jogador {
     public Carta realizarJogada(Carta carta) {     // <-- mesa.verTopoMonte()
         ArrayList<Carta> cartasPossiveis = new ArrayList();
         int i = 0;
-
+        int [] quantidadeCores = {0,0,0,0,0}; //1 Vermelho, 2 Azul, 3 Amarelo, 4 Verde
+        
         for (Carta x : this.getMao()) {    //Percorre a mão procurando cartas que podem ser jogadas           
             if (x.getCor().equals(carta.getCor()) || ((CartaNumero) x).getNumero() == ((CartaNumero) carta).getNumero()) {
                 cartasPossiveis.add(this.getMao().remove(i));
-            }
-            if (x.getCor().equals("Preto")) {
-                cartasPossiveis.add(this.getMao().remove(i));
+                
+                switch(x.getCor()){
+                    case "Vermelho":
+                        quantidadeCores[1]++;
+                        break;
+                    case "Azul":
+                        quantidadeCores[2]++;
+                        break;
+                    case "Amarelo":
+                        quantidadeCores[3]++;
+                        break;
+                    case "Verde":
+                        quantidadeCores[4]++;
+                        break;
+                    default:
+                        break;
+                }
             }
             i++;
         }
-
+        //A partir desse ponto eu ja tenho o vetor cartasPossiveis pronto.
+        
         return carta;
     }
 
