3cee0457148989c5bf1c4fbb4589852445a2f782
yosh <yosh@yosh-Inspiron-5458>
2016-07-14 16:41:44 -0400

    Continuaçao do desenvolvimento da IA

--- src/modelo/JogadorIA.java
+++ src/modelo/JogadorIA.java
@@ -19,9 +19,11 @@ public class JogadorIA extends Jogador {
 
     public Carta realizarJogada(Carta carta) {     // <-- mesa.verTopoMonte()
         ArrayList<Carta> cartasPossiveis = new ArrayList();
+        Carta cartaJogada = null;
         int i = 0;
-        int [] quantidadeCores = {0,0,0,0,0}; //1 Vermelho, 2 Azul, 3 Amarelo, 4 Verde
-        
+        Integer[] quantidadeCores = {0, 0, 0, 0, 0}; //1 Vermelho, 2 Azul, 3 Amarelo, 4 Verde
+        String [] cores = {"", "Vermelho", "Azul", "Amarelo", "Verde"};
+        while(cartasPossiveis.isEmpty()){   //Enquanto não tiver cartas para jogar
         for (Carta x : this.getMao()) {    //Percorre a mão procurando cartas que podem ser jogadas           
             if (x.getCor().equals(carta.getCor()) || ((CartaNumero) x).getNumero() == ((CartaNumero) carta).getNumero()) {
                 cartasPossiveis.add(this.getMao().remove(i));
@@ -44,10 +46,25 @@ public class JogadorIA extends Jogador {
                 }
             }
             i++;
+            //Achar uma forma de comprar carta
+        }
         }
         //A partir desse ponto eu ja tenho o vetor cartasPossiveis pronto.
+        int maiorIndice, maior;
+        maiorIndice = maior = 0;
         
-        return carta;
+        for(i = 1; i <= 4; i++){ //Acha a maior cor com mais cartas correspondentes
+            if(quantidadeCores[i] > maior){
+                maiorIndice = i;
+                maior = quantidadeCores[i];
+            }
+        }
+        for(Carta x : cartasPossiveis){ //Procura no vetor cartasDisponiveis a primeira aparição da cor para jogar
+            if(x.getCor().equals(cores[maiorIndice]))
+                cartaJogada = x;
+        }
+        
+        return cartaJogada;
     }
 
 }
