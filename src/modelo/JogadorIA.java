2e9fcf0b1963e1cc40936ec8756048568f4661ef
yosh <yosh@yosh-Inspiron-5458>
2016-07-15 20:56:39 -0400

    Corrigido a lógica da IA. Agora ela compra uma vez e passa o turno.

--- src/modelo/JogadorIA.java
+++ src/modelo/JogadorIA.java
@@ -18,7 +18,7 @@ public class JogadorIA extends Jogador {
         super(nome, mesa);
     }
 
-    public Carta realizarJogada(Carta cartaTopoMonte) {     // <-- mesa.verTopoMonte()
+    public void realizarJogada(Carta cartaTopoMonte) {     // <-- mesa.verTopoMonte()
         ArrayList<Carta> cartasPossiveis = new ArrayList(); //Vetor que guarda todas as cartas possiveis de serem jogadas
         Carta cartaJogada = null;
         Carta cartaAtual;
@@ -26,15 +26,35 @@ public class JogadorIA extends Jogador {
         Integer[] quantidadeCores = {0, 0, 0, 0, 0}; //1 Vermelho, 2 Azul, 3 Amarelo, 4 Verde
         String[] cores = {"", "Vermelho", "Azul", "Amarelo", "Verde"};
 
-        //Arrumar ConcurrentModificationException
-        do {   //Enquanto não tiver cartas para jogar não sai do loop
-            //Percorre a mão procurando cartas que podem ser jogadas           
-            for (int i = 0; i < this.mao.size(); i++) {
-                cartaAtual = this.mao.get(i);
+        //Percorre a mão procurando cartas que podem ser jogadas           
+        for (int i = 0; i < this.mao.size(); i++) {
+            cartaAtual = this.mao.get(i);
 
-                if (cartaAtual.getCor().equals(cartaTopoMonte.getCor())) {    //Verifica pela cor, serve para Simbolo/Numero de cores iguais
+            if (cartaAtual.getCor().equals(cartaTopoMonte.getCor())) {    //Verifica pela cor, serve para Simbolo/Numero de cores iguais
+                cartasPossiveis.add(cartaAtual);
+
+                switch (cartaAtual.getCor()) {       //Incrementa o vetor de quantidade de cartas
+                    case "Vermelho":
+                        quantidadeCores[1]++;
+                        break;
+                    case "Azul":
+                        quantidadeCores[2]++;
+                        break;
+                    case "Amarelo":
+                        quantidadeCores[3]++;
+                        break;
+                    case "Verde":
+                        quantidadeCores[4]++;
+                        break;
+                    default:
+                        break;
+                }
+
+                this.mao.remove(i);
+                --i; //Para não pular 1 posição no vetor apos a remoção
+            } else if (cartaAtual instanceof CartaNumero && cartaTopoMonte instanceof CartaNumero) { //Verifica se a carta é do tipo CartaNumero
+                if (((CartaNumero) cartaAtual).getNumero() == ((CartaNumero) cartaTopoMonte).getNumero()) { //Pega os números possiveis só que de cores diferentes
                     cartasPossiveis.add(cartaAtual);
-
                     switch (cartaAtual.getCor()) {       //Incrementa o vetor de quantidade de cartas
                         case "Vermelho":
                             quantidadeCores[1]++;
@@ -51,63 +71,41 @@ public class JogadorIA extends Jogador {
                         default:
                             break;
                     }
-
                     this.mao.remove(i);
-                    --i; //Para não pular 1 posição no vetor apos a remoção
-                } else if (cartaAtual instanceof CartaNumero && cartaTopoMonte instanceof CartaNumero) { //Verifica se a carta é do tipo CartaNumero
-                    if (((CartaNumero) cartaAtual).getNumero() == ((CartaNumero) cartaTopoMonte).getNumero()) { //Pega os números possiveis só que de cores diferentes
-                        cartasPossiveis.add(cartaAtual);
-                        switch (cartaAtual.getCor()) {       //Incrementa o vetor de quantidade de cartas
-                            case "Vermelho":
-                                quantidadeCores[1]++;
-                                break;
-                            case "Azul":
-                                quantidadeCores[2]++;
-                                break;
-                            case "Amarelo":
-                                quantidadeCores[3]++;
-                                break;
-                            case "Verde":
-                                quantidadeCores[4]++;
-                                break;
-                            default:
-                                break;
-                        }
-                        this.mao.remove(i);
-                        --i;
-                    }
+                    --i;
+                }
+            }
+        }
+
+        if (cartasPossiveis.isEmpty()) //Se não tiver carta para jogar, compra e termina o turno
+        {
+            this.comprarCarta();
+        } else {                    //Se tiver carta, joga
+
+            //A partir desse ponto eu ja tenho o vetor cartasPossiveis pronto.
+            int maiorIndice, maior, indiceCartaJogada = 0;
+            maiorIndice = maior = 0;
+
+            for (int i = 1; i <= 4; i++) { //Acha a cor com mais cartas correspondentes
+                if (quantidadeCores[i] > maior) {
+                    maiorIndice = i;
+                    maior = quantidadeCores[i];
+                }
+            }
+            for (int i = 0; i < cartasPossiveis.size(); i++) { //Procura no vetor cartasDisponiveis a primeira aparição da cor para jogar
+                if (cartasPossiveis.get(i).getCor().equals(cores[maiorIndice])) {
+                    cartaJogada = cartasPossiveis.get(i);
+                    cartasPossiveis.remove(i);
+                    break;
                 }
             }
 
-            if (cartasPossiveis.isEmpty()) {
-                this.comprarCarta();
-            } else {
-                break;
+            for (int i = 0; i < cartasPossiveis.size(); i++) {
+                this.mao.add(cartasPossiveis.get(i));
             }
-        } while (true);
-        //A partir desse ponto eu ja tenho o vetor cartasPossiveis pronto.
-        int maiorIndice, maior, indiceCartaJogada = 0;
-        maiorIndice = maior = 0;
 
-        for (int i = 1; i <= 4; i++) { //Acha a cor com mais cartas correspondentes
-            if (quantidadeCores[i] > maior) {
-                maiorIndice = i;
-                maior = quantidadeCores[i];
-            }
+            this.mesa.colocaNoTopoMonte(cartaJogada);
         }
-        for (int i = 0; i < cartasPossiveis.size(); i++) { //Procura no vetor cartasDisponiveis a primeira aparição da cor para jogar
-            if (cartasPossiveis.get(i).getCor().equals(cores[maiorIndice])) {
-                cartaJogada = cartasPossiveis.get(i);
-                cartasPossiveis.remove(i);
-                break;
-            }
-        }
-
-        for (int i = 0; i < cartasPossiveis.size(); i++) {
-            this.mao.add(cartasPossiveis.get(i));
-        }
-
-        return cartaJogada;
     }
 
 }
